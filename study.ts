b8d4a8750c4575e1b189f1c21b0a5bdcb121c1a7
Minhee Seo <hwarakhada@gmail.com>
2022-05-03 16:00:55 +0900

    제네릭 두개 사용

--- study.ts
+++ study.ts
@@ -1,12 +1,12 @@
 type SuperPrint = {
-    <T>(arr : T[]) : T
+    <T,M>(arr : T[], b:M) : T
 }
 
 const superPrint : SuperPrint = (arr) => {
     return arr[0]
 }
 
-const a = superPrint([1,2,3])
-const b = superPrint([true, false, true])
-const c = superPrint(["hello","hi","no"])
-const d = superPrint(["hello","hi","no",false, true])
+const a = superPrint([1,2,3],"")
+const b = superPrint([true, false, true],1)
+const c = superPrint(["hello","hi","no"],true)
+const d = superPrint(["hello","hi","no",false, true],34)
