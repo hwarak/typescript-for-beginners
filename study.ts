a5182fc91a7ec149c861500e4fb804e1ea5cb788
Minhee Seo <hwarakhada@gmail.com>
2022-05-03 10:23:15 +0900

    Tuple 사용해보기

--- study.ts
+++ study.ts
@@ -1,18 +1,1 @@
-type Age = number;
-type Name = string;
-type Player = {
-    name: Name,
-    readonly  age?: Age
-}
-
-
-// function Playermaker(name : string) : Player{
-//  return{
-//      name
-//  }
-// }
-
-const Playermaker = (name : string) : Player => ({name})
-
-const minhee = Playermaker("minhee")
-minhee.age = 12 //에러생김
+const player : [string, number, boolean] = ["hwarak" ,10, true]
