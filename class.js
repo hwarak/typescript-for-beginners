422f977ba4bb2e3cc4fb7cf29f79ee19803944cc
Minhee Seo <hwarakhada@gmail.com>
2022-05-04 08:51:05 +0900

    객체지향 이해하기 (1) 클래스 생성

---
+++ class.js
@@ -0,0 +1,16 @@
+'use strict'
+
+
+class Person{
+    // constructor
+    constructor(name, age) {
+        // fields
+        this.name = name;
+        this.age = age;
+    }
+
+    // methods
+    speak() {
+        console.log(`${this.name}: hello!`)
+    }
+}
