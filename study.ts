5af36de2cfe2c686a23a17fc3cf278863b9d8a85
minhee <hwarakhada@gmail.com>
2022-05-06 11:42:25 +0900

    클래스로 타입 만들기

--- study.ts
+++ study.ts
@@ -1,24 +1,45 @@
-abstract class User{
-    constructor(
-        protected firstName:string,
-        protected lastName:String,
-        protected nickName:string
-    ){}
+type Words = {
+  [key: string]: string;
+};
 
-    abstract getNickname():void
-
-    getFullName(){
-        return `${this.firstName} ${this.lastName}`
+class Dict {
+  private words: Words;
+  constructor() {
+    this.words = {};
+  }
+  add(word: Word) {
+    if (this.words[word.term] === undefined) {
+      this.words[word.term] = word.def;
     }
+  }
+
+  def(term: string) {
+    return this.words[term];
+  }
+
+  del(term: string) {
+    if (this.words[term] === undefined) {
+      console.log("Does not exist");
+    } else {
+      delete this.words[term];
+    }
+  }
+
+  update(word: Word) {
+    if (this.words[word.term] === undefined) {
+      console.log("Does not exist");
+    } else {
+      this.words[word.term] = word.def;
+    }
+  }
 }
 
-class Player extends User{
-    getNickname(){
-        console.log(`${this.nickName}`)
-    }
-    
+class Word {
+  constructor(public term: string, public def: string) {}
 }
 
-const hwarak = new Player("minhee", "seo", "hwarak")
-hwarak.getFullName();
+const kimchi = new Word("kimchi", "한국의 음식");
 
+const dict = new Dict();
+dict.add(kimchi);
+dict.def("kimchi");
