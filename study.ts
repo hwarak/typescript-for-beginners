22532f5294fd4c61e913afcb31588d2fbd5dbf14
minhee <hwarakhada@gmail.com>
2022-05-06 12:10:53 +0900

    type alias 제한해서 사용

--- study.ts
+++ study.ts
@@ -1,45 +1,11 @@
-type Words = {
-  [key: string]: string;
+type Team = "red" | "blue" | "yellow";
+
+type Player = {
+  nickname: string;
+  team: Team;
 };
 
-class Dict {
-  private words: Words;
-  constructor() {
-    this.words = {};
-  }
-  add(word: Word) {
-    if (this.words[word.term] === undefined) {
-      this.words[word.term] = word.def;
-    }
-  }
-
-  def(term: string) {
-    return this.words[term];
-  }
-
-  del(term: string) {
-    if (this.words[term] === undefined) {
-      console.log("Does not exist");
-    } else {
-      delete this.words[term];
-    }
-  }
-
-  update(word: Word) {
-    if (this.words[word.term] === undefined) {
-      console.log("Does not exist");
-    } else {
-      this.words[word.term] = word.def;
-    }
-  }
-}
-
-class Word {
-  constructor(public term: string, public def: string) {}
-}
-
-const kimchi = new Word("kimchi", "한국의 음식");
-
-const dict = new Dict();
-dict.add(kimchi);
-dict.def("kimchi");
+const hwarak: Player = {
+  nickname: "hwarak",
+  team: "blue",
+};
